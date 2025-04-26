3bc22f4915a4b95760f86c5d4cc2f4d1af463c7c
walissonmateus10 <walissonmateus1507@gmail.com>
2025-04-26 11:21:37 -0300

    meu primeiro commit

---
+++ core/pessoa.py
@@ -0,0 +1,21 @@
+class Pessoa:
+    def  __init__(self):
+        self.nome = ""
+        self.idade = ""
+
+    def get_nome(self):
+            return self.nome
+
+
+    def get_idade(self):
+                return self.idade
+        
+    def get_dados(self):
+            return self.nome, idade
+
+    def set_dados(self, nome, idade):
+            self.nome = nome
+            self.idade = idade
+    
+
+        
